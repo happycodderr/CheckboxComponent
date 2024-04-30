95f717bf0faed0ee4e86cf204381a21ebd784631
GG <geeonmn072020@gmail.com>
2024-04-30 18:14:47 +0100

    Add checkbox component

---
+++ CheckboxComponent/CheckboxComponent/CheckboxComponentApp.swift
@@ -0,0 +1,11 @@
+
+import SwiftUI
+
+@main
+struct CheckboxComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TermsView(viewModel: TermsViewModel())
+        }
+    }
+}
---
+++ CheckboxComponent/CheckboxComponent/TermsViewModel.swift
@@ -0,0 +1,22 @@
+import Combine
+import SwiftUI
+
+final class TermsViewModel: ObservableObject {
+    @Published var termsConditions: [TermsConditions] = [
+        TermsConditions(label: "Terms One", isChecked: true),
+        TermsConditions(label: "Terms Two", isChecked: false),
+    ] {
+        didSet {
+            buttonState = termsConditions.allSatisfy({ $0.isChecked }) ? .enabled : .disabled
+        }
+    }
+    @Published var buttonState: ButtonState = .disabled
+    private var cancellables: Set<AnyCancellable> = []
+}
+
+struct TermsConditions: Identifiable {
+    var id = UUID()
+    
+    let label: String
+    var isChecked: Bool
+}
